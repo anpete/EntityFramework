3bedfb56985059e945b07963bc4343d717c17a0e
Andrew Peters <anpete@microsoft.com>
2018-02-16 13:04:34 -0800

    WIP

---
+++ src/EFCore.Analyzers/DiagnosticDescriptors.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.CodeAnalysis;
+
+namespace Microsoft.AspNetCore.Mvc.Analyzers
+{
+    public static class DiagnosticDescriptors
+    {
+        public static readonly DiagnosticDescriptor EF1000_ApiActionsShouldReturnActionResultOf =
+            new DiagnosticDescriptor(
+                "MVC1002",
+                "Actions on types annotated with ApiControllerAttribute should return ActionResult<T>.",
+                "Actions on types annotated with ApiControllerAttribute should return ActionResult<T>.",
+                "Usage",
+                DiagnosticSeverity.Warning,
+                isEnabledByDefault: true);
+    }
+}
